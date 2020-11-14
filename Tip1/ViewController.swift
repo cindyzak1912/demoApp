2318e2afd09a8c5975da10cbf4be5b24f44ddc7e
cindyzak1912 <56843991+cindyzak1912@users.noreply.github.com>
2020-11-14 13:49:15 -0500

    Initial Commit

---
+++ Tip1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip1
+//
+//  Created by cindy zak on 11/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
